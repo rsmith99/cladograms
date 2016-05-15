df72bdbb2537599f8b1a54deb2af08ffe3d755cb
Barney <barney@tbotix.com>
2016-05-15 09:52:52 +0100

    added contact page with google map

--- SharkWebsite/cladogram1.js
+++ SharkWebsite/cladogram1.js
@@ -7,8 +7,8 @@ function draw_chart() {
 
 	var node_ids = ['elasmo','hexan','heter','oreet','squal','lamni','charc','squan',
 					'echni','prist']
-	var node_coords = [[480,110],[360,200],[720,200],[100,300],[800,300],[200,400],
-						[400,400],[850,400],[900,500],[1100,500]]
+	var node_coords = [[480,90],[360,170],[720,170],[100,300],[800,300],[200,400],
+						[400,400],[850,400],[900,460],[1100,460]]
 
 	for(i=0;i<node_ids.length;i++) {
 		d = document.getElementById(node_ids[i])
@@ -18,12 +18,12 @@ function draw_chart() {
 	};
 
 
-  	coords = [[600,100], [780,100], [780,130], [780,100], [900,100], [900,200], [860,200],
-  				[860,230], [860,200], [960,200], [960,300], [910,300], [910,330], [910,300],
-  				[1065,300], [1065,400], [980,400], [980,430], [980,400], [1180,400], [1180,430],
-  				[600,100], [420,100], [420,130], [420,100], [300,100], [300,200], [200,200],
-  				[200,230], [200,200], [360,200], [360,300], [270,300], [270,330], [270,300],
-  				[480,300], [480,330]]
+  	coords = [[600,100], [780,100], [780,130], [780,100], [900,100], [900,250], [860,250],
+  				[860,300], [860,250], [960,250], [960,400], [910,400], [910,430], [910,400],
+  				[1065,400], [1065,500], [980,500], [980,550], [980,500], [1180,500], [1180,550],
+  				[600,100], [420,100], [420,130], [420,100], [300,100], [300,250], [200,250],
+  				[200,300], [200,250], [360,250], [360,400], [270,400], [270,450], [270,400],
+  				[480,400], [480,450]]
 
   	ctx.moveTo(600, 60);
   	ctx.strokeStyle='white';
@@ -38,5 +38,16 @@ function draw_chart() {
   		ctx.lineTo(x, y);
   	};
 	ctx.stroke();
+
+	document.getElementById("charc").onmouseover = function() {mouseOver()};
+
+	function mouseOver() {
+    	document.getElementById("info-title").innerHTML = Carcharniniformes_title;
+    	document.getElementById("info-description").innerHTML = Carcharniniformes_Description;
+    	document.getElementById('info-image').src = 'http://imgc.allpostersimages.com/images/P-473-488-90/69/6997/L77W100Z/posters/fishes-carcharhiniformes-school-of-sharks-galeorhinus-galeus-illustration.jpg'
+
+	}
+
+
 
 };
